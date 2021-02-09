9df9f784db4c5caea620d65b42f57128f34fa65e
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-09 10:34:43 -0600

    Changed heading to have it display last and first name

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Jose";
+const lName = "Alvarez";
 const number = 21;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
+    <h1>
+      Hello {name} {lName}!
+    </h1>
     <p> My lucky number is {number} </p>
   </div>,
   document.getElementById("root")
